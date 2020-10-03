df8ff04d4e0f8d866ab582ac791b7e10d582379b
j0thu <jonathanben215@gmail.com>
2020-10-04 00:57:12 +0530

    DLR

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import {Link} from 'react-router-dom';
 
-const Login = ()=>{
+const Login = ({setAuth})=>{
+
+    const [inputs, setInputs] = useState({
+        email:"",
+        password:""
+    });
+
+    const {email, password} = inputs;
+
+    const onChange = (e)=>{
+        setInputs({...inputs, [e.target.name]:e.target.value});
+    }
+
+    const onSubmitForm = async(e)=>{
+        e.preventDefault();
+        try {
+
+            const body = {email, password};
+
+            const response = await fetch("http://localhost:5000/auth/login", {
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body: JSON.stringify(body),
+            });
+            
+            const parseResponse = await response.json();
+            localStorage.setItem("token", parseResponse.token);
+            setAuth(true);
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return (
         <Fragment>
-            <h1>Login</h1>
+            <h1 className="my-3">Login</h1>
+            <form action="" onSubmit={onSubmitForm}>
+                <input type="email" name="email" placeholder="E-mail" className="form-control my-3 w-50" value={email} onChange = {e=>onChange(e)}/>
+                <input type="password" name ="password" placeholder="Password" className="form-control my-3 w-50" value={password} onChange = {e=>onChange(e)}/>
+                <button className="btn btn-success btn-block w-50">Login</button>
+            </form>
+            <Link to ='/register'>Register</Link>
         </Fragment>
     )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,15 +1,48 @@
 import React, { Fragment, useState } from 'react';
+import {Link} from 'react-router-dom';
 
-const Register = ()=>{
+const Register = ({setAuth})=>{
+
+    const [inputs, setInputs] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const {name, email, password} = inputs;
+
+    const onChange = (e)=>{
+        setInputs({...inputs, [e.target.name]:e.target.value});
+    }
+
+    const onSubmitForm = async(e)=>{ //on submit, after insertion of data in the db, you will receive a token
+        e.preventDefault();
+        try {
+            const body = {name, email, password}
+            const response = await fetch("http://localhost:5000/auth/register", {
+                method:"POST",
+                headers: {"Content-Type":"application/json"},
+                body:JSON.stringify(body)
+            })
+        const parseResponse = await response.json();
+        localStorage.setItem("token", parseResponse.token); //setting the token to localstorage
+        setAuth(true);
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return (
         <Fragment>
-            <h1>Register</h1>
-            <form action="">
-                <input type="text" name ="name" placeholder="Name" className="form-control my-3"/>
-                <input type="email" name ="email" placeholder="e-mail" className="form-control my-3"/>
-                <input type="password" name ="password" placeholder="password" className="form-control my-3"/>
-                <button className="btn btn-success btn-block">Register</button>
+            <h1 className="my-3">Register</h1>
+            <form action="" onSubmit = {onSubmitForm}>
+                <input type="text" name ="name" placeholder="Name" className="form-control my-3 w-50" value={name} onChange={e=>onChange(e)}/>
+                <input type="email" name ="email" placeholder="E-mail" className="form-control my-3 w-50" value={email} onChange={e=>onChange(e)}/>
+                <input type="password" name ="password" placeholder="Password" className="form-control my-3 w-50" value={password} onChange={e=>onChange(e)}/>
+                <button className="btn btn-success btn-block w-50">Register</button>
             </form>
+            <Link to='/login'>Login</Link>
         </Fragment>
     )
 }
