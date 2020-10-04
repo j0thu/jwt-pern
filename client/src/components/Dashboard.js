66d698991736c0ab27aee1c048f07bf446a03a46
j0thu <jonathanben215@gmail.com>
2020-10-04 20:10:52 +0530

    dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 const Dashboard = ({setAuth})=>{
     
+    const [name, setName] = useState("");
+
+    async function getName() {
+        try {
+            const response = await fetch("http://localhost:5000/dashboard", {
+                method:"GET",
+                headers:{token:localStorage.token}, //we are setting the token to the header
+            })
+            const parseResponse = await response.json();
+            // console.log(parseResponse); -> will give you the user_name in the console.
+            setName(parseResponse.user_name); //setting the user_name received in the console to the state
+        }
+        catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    function logout(e){
+        e.preventDefault();
+        localStorage.removeItem('token');
+        setAuth(false);
+    }
+
+
+    useEffect(()=>{
+        getName();
+    })
+
     return (
         <Fragment>
             <h1>Dashboard</h1>
-            <button onClick = {()=>setAuth(false)}>Logout</button>
+            <h3>Hello {name}</h3>
+            <button className="btn btn-info" onClick={e=>logout(e)}>Logout</button>
         </Fragment>
     )
 }
